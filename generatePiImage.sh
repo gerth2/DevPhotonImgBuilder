0f52be63dcfb9b0d5d86c81e14f20605d3be521d
Chris Gerth <chrisgerth010592@gmail.com>
2021-12-21 20:02:47 -0600

    another one

--- generatePiImage.sh
+++ generatePiImage.sh
@@ -1,5 +1,7 @@
 PI_BASE_IMG_TAG=v2021.1.4
 PHOTONVISION_RELEASE_TAG=v2022.1.1-beta-2
+VENDOR_PREFIX=snakeyes
+VENDOR_RELEASE=${RELEASE_VERSION}
 
 # Install dependencies
 sudo apt install unzip zip
@@ -29,8 +31,11 @@ sudo cp $JAR_FILE_NAME photonvision.jar
 popd
 sudo umount ${TMP}
 sudo rmdir ${TMP}
-rm $IMG_FILE_NAME
-NEW_IMAGE=$(basename "${JAR_FILE_NAME/jar/img}")
+NEW_IMAGE=$(basename "${VENDOR_PREFIX}-${VENDOR_RELEASE}.img")
 mv $IMAGE_FILE $NEW_IMAGE
-zip -r $(basename "${JAR_FILE_NAME/.jar/-image.zip}") $NEW_IMAGE
+zip -r $(basename "${VENDOR_PREFIX}-${VENDOR_RELEASE}-image.zip") $NEW_IMAGE
+ls
 rm $NEW_IMAGE
+rm $JAR_FILE_NAME
+rm $IMG_FILE_NAME
+
